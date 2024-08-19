c6115a989030dca2129f3e23c27f47f75288aeb3
StudiesGitHub <ncsaraf@outlook.com>
2024-08-19 19:24:23 -0300

    up

--- nodejs/aulas/desafios/desafio1/src/database.js
+++ nodejs/aulas/desafios/desafio1/src/database.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs/promises'
+
+const databasePath = new URL('../db.json', import.meta.url) //essa linha faz com que o db.js sempre seja criado no root
+
 export class Database {
     #database = {}
+
+    //o constructor abaixo garante que o db seja "re-buscado" quando rodamos o servidor novamente.
+    constructor() {
+        fs.readFile(databasePath, 'utf8')
+          .then(data => {
+            this.#database = JSON.parse(data)
+          })
+          .catch(() => {
+            this.#persist()
+          })
+      }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -13,6 +32,8 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+
+        this.#persist();
 
         return data;
     }
