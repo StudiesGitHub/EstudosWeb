a342a39c5bcb69904492fbffb6c7aa759b192346
StudiesGitHub <ncsaraf@outlook.com>
2024-08-26 10:26:35 -0300

    up

--- nodejs/aulas/desafios/desafio2/src/server.ts
+++ nodejs/aulas/desafios/desafio2/src/server.ts
@@ -2,8 +2,32 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/meal', () => {
+  return 'Refeicoes listadas'
+})
+
+app.post('/meal', () => {
+  const meal = {
+    IdUser: '1',
+    name: 'AO MOSSO',
+    description: 'description1',
+    dateAndTime: '22/04/2024 10:15 AM',
+    onTheDiet: 'true',
+  }
+
+  return meal
+})
+
+app.post('/meal/user', () => {
+  return 'Usuario criado'
+})
+
+app.patch('/meal/update', () => {
+  return 'Refeicao atualizada '
+})
+
+app.delete('/meal/remove', () => {
+  return 'Refeicao removida'
 })
 
 app
